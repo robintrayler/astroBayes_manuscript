62748b08134a6c1b32f419469045c9013a2b700f
Robin B. Trayler <robintrayler@gmail.com>
2023-04-12 14:51:32 -0700

    update hiatus figure

--- R/CIP2_hiatus_sensitivity.R
+++ R/CIP2_hiatus_sensitivity.R
@@ -1,21 +1,21 @@
 # load required libraries -----------------------------------------------------
 library(tidyverse)
-library(astroBayes)
-library(viridis)
+library(cowplot)
+
 source('./R/plot_settings.R')
 # load all the data -----------------------------------------------------------
 two_ages <- list.files('./results/random_age_validation/CIP2_2_ages',
-                        full.names = TRUE, 
-                        pattern = '*.rds')
-four_ages <- list.files('./results/random_age_validation/CIP2_4_ages',
                        full.names = TRUE, 
                        pattern = '*.rds')
+four_ages <- list.files('./results/random_age_validation/CIP2_4_ages',
+                        full.names = TRUE, 
+                        pattern = '*.rds')
 six_ages <- list.files('./results/random_age_validation/CIP2_6_ages',
                        full.names = TRUE, 
                        pattern = '*.rds')
 eight_ages <- list.files('./results/random_age_validation/CIP2_8_ages',
-                       full.names = TRUE, 
-                       pattern = '*.rds')
+                         full.names = TRUE, 
+                         pattern = '*.rds')
 
 file_list <- c(two_ages, four_ages, six_ages, eight_ages)
 
@@ -38,16 +38,13 @@ for(i in seq_along(file_list)) {
   storage$delta[i] <- (model$geochron_data$position - hiatus_position) |> 
     abs() |> 
     min()
-
+  
   rm(model)
 }
 
-
-pdf(file = './figures/hiatus_duration.pdf',
-    width = 4,
-    height = 4)
-storage |> 
-  filter(median != 0) |> 
+hiatus_2 <-  storage |> 
+  filter(n_ages == '2 ages') |> 
+  filter(median !=0) |> 
   ggplot(mapping = aes(x = delta, 
                        y = median,
                        ymax = CI_97.5,
@@ -55,13 +52,89 @@ storage |>
   geom_point(alpha = 0.1,
              size = 0.5) + 
   geom_linerange(alpha = 0.05, 
-                  show.legend = FALSE) + 
+                 show.legend = FALSE) + 
   ylim(0, 0.4) + 
-  facet_wrap(~n_ages) + 
+  geom_hline(yintercept = 0.2025,
+                            linetype = 'dashed',
+                            color = 'red',
+                            linewidth = 1) + 
+  xlab(expression(Delta*'hiatus-date (m)')) + 
+  ylab('hiatus duration (Ma)') + 
+  theme(axis.title.x = element_blank(),
+        axis.text.x = element_blank())
+
+hiatus_4 <-  storage |> 
+  filter(n_ages == '4 ages') |> 
+  filter(median !=0) |> 
+  ggplot(mapping = aes(x = delta, 
+                       y = median,
+                       ymax = CI_97.5,
+                       ymin = CI_2.5)) + 
+  geom_point(alpha = 0.1,
+             size = 0.5) + 
+  geom_linerange(alpha = 0.05, 
+                 show.legend = FALSE) + 
+  ylim(0, 0.4) + 
+  geom_hline(yintercept = 0.2025,
+             linetype = 'dashed',
+             color = 'red',
+             linewidth = 1) + 
+  xlab(expression(Delta*'hiatus-date (m)')) + 
+  ylab('hiatus duration (Ma)') + 
+  theme(axis.title.x = element_blank(), 
+        axis.title.y = element_blank(),
+        axis.text = element_blank())
+
+hiatus_6 <-  storage |> 
+  filter(n_ages == '6 ages') |> 
+  filter(median !=0) |> 
+  ggplot(mapping = aes(x = delta, 
+                       y = median,
+                       ymax = CI_97.5,
+                       ymin = CI_2.5)) + 
+  geom_point(alpha = 0.1,
+             size = 0.5) + 
+  geom_linerange(alpha = 0.05, 
+                 show.legend = FALSE) + 
+  ylim(0, 0.4) + 
   geom_hline(yintercept = 0.2025,
              linetype = 'dashed',
              color = 'red',
              linewidth = 1) + 
   xlab(expression(Delta*'hiatus-date (m)')) + 
   ylab('hiatus duration (Ma)')
+
+hiatus_8 <-  storage |> 
+  filter(n_ages == '8 ages') |> 
+  filter(median !=0) |> 
+  ggplot(mapping = aes(x = delta, 
+                       y = median,
+                       ymax = CI_97.5,
+                       ymin = CI_2.5)) + 
+  geom_point(alpha = 0.1,
+             size = 0.5) + 
+  geom_linerange(alpha = 0.05, 
+                 show.legend = FALSE) + 
+  ylim(0, 0.4) + 
+  geom_hline(yintercept = 0.2025,
+             linetype = 'dashed',
+             color = 'red',
+             linewidth = 1) + 
+  xlab(expression(Delta*'hiatus-date (m)')) + 
+  ylab('hiatus duration (Ma)') + 
+  theme(axis.text.y = element_blank(),
+        axis.title.y = element_blank())
+  
+
+
+
+
+pdf(file = './figures/hiatus_duration.pdf',
+    width = 4,
+    height = 4)
+plot_grid(hiatus_2, hiatus_4, hiatus_6, hiatus_8,
+          align = 'hv',
+          labels = 'AUTO',
+          label_x = 0.22,
+          label_y = 0.99)
 dev.off()
