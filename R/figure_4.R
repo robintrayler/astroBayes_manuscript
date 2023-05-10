bea248f2955dc4def56e9c4ae6c3ac37fd4e83e1
Robin B. Trayler <robintrayler@gmail.com>
2023-05-10 14:52:30 -0700

    aded plots

--- R/figure_4.R
+++ R/figure_4.R
@@ -2,161 +2,92 @@ library(tidyverse)
 source('./R/plot_settings.R')
 library(cowplot)
 library(astroBayes)
-
+library(modifiedBChron)
 # load required data ----------------------------------------------------------
 TD1_true <- read_csv('./data/TD1/true_age.csv')
 CIP2_true <- read_csv('./data/CIP2/true_age.csv')
 
-
+make_plot <- function(rds, xlim = c(0, 2.1), 
+                      ylim = c(20, 0)) {
+  dates <- rds$geochron_data
+  range <- -seq(min(rds$segment_edges$position), max(rds$segment_edges$position), length = 500)
+  # make a bchron model
+  bmodel <- ageModel(ages = dates$age,
+                     ageSds = dates$age_sd,
+                     positions = -dates$position,
+                     ids = dates$id,
+                     predictPositions = range,
+                     truncateUp = -1e10)
+  
+  # make the plot
+  p <- plot(rds, 
+            type = 'age_depth') + 
+    theme_custom + 
+    theme(legend.position = 'none') + 
+    xlab('Age (Ma)') + 
+    ylab('Depth (m)') + 
+    geom_line(data = TD1_true,
+              mapping = aes(x = age, 
+                            y = position),
+              inherit.aes = FALSE,
+              color = 'red',
+              linetype = 'dotted') + 
+    xlim(xlim) +
+    ylim(ylim) +
+    theme(axis.title = element_text(size = 12),
+          axis.text = element_text(size = 10))
+  
+  # add bchron reference lines
+  CI <- bmodel$HDI |> 
+    t() |> 
+    as.data.frame() 
+  
+  CI$depth <- -bmodel$predictPositions
+  
+  p <- p + 
+    geom_line(data = CI,
+              aes(x = `50%`, y = depth),
+              inherit.aes = FALSE,
+              linetype = 'solid',
+              color = 'grey75') + 
+    geom_line(data = CI,
+              aes(x = `2.5%`, y = depth),
+              inherit.aes = FALSE,
+              linetype = 'dashed',
+              color = 'grey75') +  
+    geom_line(data = CI,
+              aes(x = `97.5%`, y = depth),
+              inherit.aes = FALSE,
+              linetype = 'dashed',
+              color = 'grey75')
+  
+  
+}
 ###############################################################################
 # TD1 models ##################################################################
 ###############################################################################
 # 2 ages ----------------------------------------------------------------------
-TD1_2 <- read_rds(file = './results/random_age_validation/TD1_2_ages/2_age_model_983.rds')
-TD_model_2_plot <- plot(TD1_2, type = 'age_depth') + 
-  theme_custom + 
-  theme(legend.position = 'none') + 
-  xlab('Age (Ma)') + 
-  ylab('Depth (m)') + 
-  geom_line(data = TD1_true,
-            mapping = aes(x = age, 
-                          y = position),
-            inherit.aes = FALSE,
-            color = 'red',
-            linetype = 'dashed') + 
-  xlim(0, 2.05) + 
-  ylim(20, 0) + 
-  theme(axis.title.x = element_blank(),
-        axis.text.x = element_blank())
-
-# 4 ages ----------------------------------------------------------------------
-TD1_4 <- read_rds(file = './results/random_age_validation/TD1_4_ages/4_age_model_273.rds')
-TD_model_4_plot <- plot(TD1_4, type = 'age_depth') + 
-  theme_custom + 
-  theme(legend.position = 'none') + 
-  xlab('Age (Ma)') + 
-  ylab('Depth (m)') + 
-  geom_line(data = TD1_true,
-            mapping = aes(x = age, 
-                          y = position),
-            inherit.aes = FALSE,
-            color = 'red',
-            linetype = 'dashed') + 
-  xlim(0, 2.05) + 
-  ylim(20, 0) + 
-  theme(axis.title.x = element_blank(),
-        axis.text.x = element_blank())
-
-# 6 ages ----------------------------------------------------------------------
-TD1_6 <- read_rds(file = './results/random_age_validation/TD1_6_ages/6_age_model_226.rds')
-TD_model_6_plot <- plot(TD1_6, type = 'age_depth') + 
-  theme_custom + 
-  theme(legend.position = 'none') + 
-  xlab('Age (Ma)') + 
-  ylab('Depth (m)') + 
-  geom_line(data = TD1_true,
-            mapping = aes(x = age, 
-                          y = position),
-            inherit.aes = FALSE,
-            color = 'red',
-            linetype = 'dashed') + 
-  xlim(0, 2.05) + 
-  ylim(20, 0) + 
-  theme(axis.title.x = element_blank(),
-        axis.text.x = element_blank())
-
-# 8 ages ----------------------------------------------------------------------
-TD1_8 <- read_rds(file = './results/random_age_validation/TD1_8_ages/8_age_model_881.rds')
-TD_model_8_plot <- plot(TD1_8, type = 'age_depth') + 
-  theme_custom + 
-  theme(legend.position = 'none') + 
-  xlab('Age (Ma)') + 
-  ylab('Depth (m)') + 
-  geom_line(data = TD1_true,
-            mapping = aes(x = age, 
-                          y = position),
-            inherit.aes = FALSE,
-            color = 'red',
-            linetype = 'dashed') + 
-  xlim(0, 2.05) + 
-  ylim(20, 0) 
+TD1_2 <- make_plot(read_rds(file = './results/random_age_validation/TD1_2_ages/2_age_model_983.rds'))
+TD1_4 <- make_plot(read_rds(file = './results/random_age_validation/TD1_4_ages/4_age_model_273.rds'))
+TD1_6 <- make_plot(read_rds(file = './results/random_age_validation/TD1_6_ages/6_age_model_226.rds'))
+TD1_8 <- make_plot(read_rds(file = './results/random_age_validation/TD1_8_ages/8_age_model_881.rds'))
 
 ###############################################################################
 # CIP2 models #################################################################
 ###############################################################################
 # 2 ages ----------------------------------------------------------------------
-CIP_model_2 <- read_rds(file = './results/random_age_validation/CIP2_2_ages/2_age_model_983.rds')
-CIP_model_2_plot <- plot(CIP_model_2, type = 'age_depth') + 
-  theme_custom + 
-  theme(legend.position = 'none') + 
-  xlab('Age (Ma)') + 
-  ylab('Depth (m)') + 
-  geom_line(data = CIP2_true,
-            mapping = aes(x = age, 
-                          y = position),
-            inherit.aes = FALSE,
-            color = 'red',
-            linetype = 'dashed') + 
-  xlim(0.45, 1.55) +
-  ylim(10, -1.5) + 
-  theme(axis.title.x = element_blank(),
-        axis.text.x = element_blank(),
-        axis.title.y = element_blank())
-
-# 4 ages ----------------------------------------------------------------------
-CIP_model_4 <- read_rds(file = './results/random_age_validation/CIP2_4_ages/4_age_model_944.rds')
-CIP_model_4_plot <- plot(CIP_model_4, type = 'age_depth') + 
-  theme_custom + 
-  theme(legend.position = 'none') + 
-  xlab('Age (Ma)') + 
-  ylab('Depth (m)') + 
-  geom_line(data = CIP2_true,
-            mapping = aes(x = age, 
-                          y = position),
-            inherit.aes = FALSE,
-            color = 'red',
-            linetype = 'dashed') + 
-  xlim(0.45, 1.55) +
-  ylim(10, -1.5) + 
-  theme(axis.title.x = element_blank(),
-        axis.text.x = element_blank(),
-        axis.title.y = element_blank())
-
-# 6 ages ----------------------------------------------------------------------
-CIP_model_6 <- read_rds(file = './results/random_age_validation/CIP2_6_ages/6_age_model_400.rds')
-CIP_model_6_plot <- plot(CIP_model_6, type = 'age_depth') + 
-  theme_custom + 
-  theme(legend.position = 'none') + 
-  xlab('Age (Ma)') + 
-  ylab('Depth (m)') + 
-  geom_line(data = CIP2_true,
-            mapping = aes(x = age, 
-                          y = position),
-            inherit.aes = FALSE,
-            color = 'red',
-            linetype = 'dashed') + 
-  xlim(0.45, 1.55) +
-  ylim(10, -1.5) + 
-  theme(axis.title.x = element_blank(),
-        axis.text.x = element_blank(),
-        axis.title.y = element_blank())
-
-# 8 ages ----------------------------------------------------------------------
-CIP_model_8 <- read_rds(file = './results/random_age_validation/CIP2_8_ages/8_age_model_210.rds')
-CIP_model_8_plot <- plot(CIP_model_8, type = 'age_depth') + 
-  theme_custom + 
-  theme(legend.position = 'none') + 
-  xlab('Age (Ma)') + 
-  ylab('Depth (m)') + 
-  geom_line(data = CIP2_true,
-            mapping = aes(x = age, 
-                          y = position),
-            inherit.aes = FALSE,
-            color = 'red',
-            linetype = 'dashed') + 
-  xlim(0.45, 1.55) +
-  ylim(10, -1.5) + 
-  theme(axis.title.y = element_blank())
+CIP_model_2 <- make_plot(read_rds(file = './results/random_age_validation/CIP2_2_ages/2_age_model_983.rds'), 
+                         xlim = c(0.45, 1.6), 
+                         ylim = c(10, -1.5))
+CIP_model_4 <- make_plot(read_rds(file = './results/random_age_validation/CIP2_4_ages/4_age_model_944.rds'), 
+                         xlim = c(0.45, 1.6), 
+                         ylim = c(10, -1.5))
+CIP_model_6 <- make_plot(read_rds(file = './results/random_age_validation/CIP2_6_ages/6_age_model_400.rds'), 
+                         xlim = c(0.45, 1.6), 
+                         ylim = c(10, -1.5))
+CIP_model_8 <- make_plot(read_rds(file = './results/random_age_validation/CIP2_8_ages/8_age_model_210.rds'), 
+                         xlim = c(0.45, 1.6), 
+                         ylim = c(10, -1.5))
 
 
 
@@ -167,9 +98,21 @@ plot_grid(TD_model_2_plot, CIP_model_2_plot,
           TD_model_4_plot, CIP_model_4_plot,
           TD_model_6_plot, CIP_model_4_plot,
           TD_model_8_plot, CIP_model_8_plot,
-  ncol = 2,
-  align = 'hv',
-  labels = c('A', 'E', 'B', 'F', 'C', 'G', 'D', 'H' ),
-  label_x = 0.85,
-  label_y = 0.97)
+          ncol = 2,
+          align = 'hv',
+          labels = c('A', 'E', 'B', 'F', 'C', 'G', 'D', 'H' ),
+          label_x = 0.85,
+          label_y = 0.97)
 dev.off()
+
+
+plot_grid(CIP_model_2_plot,
+          CIP_model_4_plot,
+          CIP_model_6_plot,
+          CIP_model_8_plot,
+          ncol = 1,
+          align = 'hv',
+          labels = c('2', '4', '6', '8', ),
+          label_x = 0.85,
+          label_y = 0.97)
+
